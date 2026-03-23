4568ecb93aa3bd3898f7981296a41be8fd677a0f
Allan Viray <allang.v.dev@gmail.com>
2026-03-23 18:46:30 -0500

    create initial Sidebar component

---
+++ components/layout/Sidebar.jsx
@@ -0,0 +1,25 @@
+const Sidebar = () => {
+  return (
+    <div>
+      <h2>(Sidebar component)</h2>
+      <nav>
+        <ul>
+          <li>
+            <a href="#">Dashboard</a>
+          </li>
+          <li>
+            <a href="#">Analytics</a>
+          </li>
+          <li>
+            <a href="#">Reports</a>
+          </li>
+          <li>
+            <a href="#">Settings</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Sidebar;
