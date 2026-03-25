ae5a551a25f653718e165f92c6fe324bd6f48b0e
Allan Viray <allang.v.dev@gmail.com>
2026-03-25 16:12:24 -0500

    create initial UserGrowthChart component

---
+++ components/UserGrowthChart.jsx
@@ -0,0 +1,10 @@
+const UserGrowthChart = () => {
+  return (
+    <div>
+      <h2>User Growth Chart</h2>
+      <div>[Chart displayed here]</div>
+    </div>
+  );
+};
+
+export default UserGrowthChart;
