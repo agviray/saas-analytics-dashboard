1758538664d12a8bbdd3cf13c8172e4b412746d7
Allan Viray <allang.v.dev@gmail.com>
2026-03-25 16:09:19 -0500

    create initial RevenueOverTimeChart component

---
+++ components/RevenueOverTimeChart.jsx
@@ -0,0 +1,10 @@
+const RevenueOverTimeChart = () => {
+  return (
+    <div className="revenue-chart">
+      <h2>Revenue Over Time Chart</h2>
+      <div className="chart-placeholder">[Chart will be displayed here]</div>
+    </div>
+  );
+};
+
+export default RevenueOverTimeChart;
