299ba9aff3c9c06411ed74cb356c8a632b3dfe3e
Allan Viray <allang.v.dev@gmail.com>
2026-03-24 20:33:12 -0500

    create initial Table component

---
+++ components/ui/Table.jsx
@@ -0,0 +1,31 @@
+const Table = () => {
+  return (
+    <table>
+      <thead>
+        <th>Header 1</th>
+        <th>Header 2</th>
+        <th>Header 3</th>
+        <th></th>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Row 1, Cell 1</td>
+          <td>Row 1, Cell 2</td>
+          <td>Row 1, Cell 3</td>
+        </tr>
+        <tr>
+          <td>Row 2, Cell 1</td>
+          <td>Row 2, Cell 2</td>
+          <td>Row 2, Cell 3</td>
+        </tr>
+        <tr>
+          <td>Row 3, Cell 1</td>
+          <td>Row 3, Cell 2</td>
+          <td>Row 3, Cell 3</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
+
+export default Table;
