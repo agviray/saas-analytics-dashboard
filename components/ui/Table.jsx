bdd1cf5c6a662b99150d7f8d6b35632b28311d1f
Allan Viray <allang.v.dev@gmail.com>
2026-04-02 18:20:12 -0500

    fix: remove unnecessary classes

--- components/ui/Table.jsx
+++ components/ui/Table.jsx
@@ -1,14 +1,11 @@
 const Table = ({ columns, data }) => {
   const renderTableData = (data, columns) => {
     return data.map((item) => (
-      <tr
-        key={item.id}
-        className="flex justify-center items-start border-b border-gray-200 w-full "
-      >
+      <tr key={item.id} className="border-b border-gray-200">
         {columns.map((col) => (
           <td
             key={`${item.id}-${col.key}`}
-            className="flex shrink-0 flex-1 p-6 py-[19.5px]  color-[#101828] text-sm font-normal leading-5 tracking-[-0.15px] w-full"
+            className="px-6 py-[19.5px] text-[#101828] text-sm font-normal leading-5 tracking-[-0.15px]"
           >
             {item[col.key]}
           </td>
@@ -19,21 +16,19 @@ const Table = ({ columns, data }) => {
 
   return (
     <table className="w-full">
-      <thead className="shrink-0 bg-gray-50 border-b border-gray-200 w-full">
-        <tr className="flex shrink-0 w-full">
+      <thead className="bg-gray-50 border-b border-gray-200">
+        <tr>
           {columns.map((col) => (
             <th
               key={col.key}
-              className="flex-1 p-6 py-3 text-left color-[#6a7282] text-xs font-medium leading-4 tracking-[.6px]"
+              className="px-6 py-3 text-left text-[#6a7282] text-xs font-medium leading-4 tracking-[.6px]"
             >
               {col.label}
             </th>
           ))}
         </tr>
       </thead>
-      <tbody className="flex flex-col justify-center items-start shrink-0 bg-white w-full">
-        {renderTableData(data, columns)}
-      </tbody>
+      <tbody className="bg-white">{renderTableData(data, columns)}</tbody>
     </table>
   );
 };
