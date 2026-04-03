f16009116343b86b9fc5f42c023caed5080e7d6f
Allan Viray <allang.v.dev@gmail.com>
2026-04-02 19:13:38 -0500

    create initial Badge component

---
+++ components/ui/Badge.jsx
@@ -0,0 +1,15 @@
+const badgeStyles = {
+  success: 'text-[#00A63E] bg-[#F0FDF4]',
+  failed: 'text-[#E7000B] bg-[#FEF2F2]',
+  pending: 'text-[#D08700] bg-[#FEFCE8]',
+};
+
+const Badge = ({ children }) => {
+  return (
+    <span className="inline-block px-2 py-1 border border-0 rounded-full">
+      {children}
+    </span>
+  );
+};
+
+export default Badge;
